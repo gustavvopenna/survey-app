35ffdf3b82e4976cfca51495fd5e8f95f5f4a56d
gustavvopenna <gustavo.pema@gmail.com>
2019-07-19 17:30:52 -0500

    Now questions change on click

--- src/screens/questionScreen.js
+++ src/screens/questionScreen.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 import Subtitle from '../components/Subtitle'
 import Button from '../components/StyledButton'
 import Reactions from '../components/Reactions'
@@ -9,7 +10,42 @@ const scope = {
   }
 }
 
+const endpoint =
+  'https://tkfn9ciovh.execute-api.us-west-1.amazonaws.com/qa/api/surveys/synchronization'
+
 class QuestionScreen extends React.Component {
+  state = {
+    questions: [],
+    currentQuestion: '',
+    questionNumber: 0
+  }
+
+  componentDidMount() {
+    axios
+      .post(endpoint)
+      .then(res => {
+        this.setState({
+          currentQuestion: res.data.questions[this.state.questionNumber],
+          questions: res.data.questions
+        })
+        console.log(this.state.questions)
+      })
+      .catch(err => console.log(err))
+  }
+
+  handleQuestion = e => {
+    this.setState((prevState, props) => ({
+      questionNumber: prevState.questionNumber + 1
+    }))
+
+    const number = this.state.questionNumber
+
+    console.log(this.state.questionNumber)
+    this.setState((prevState, props) => ({
+      currentQuestion: prevState.questions[number]
+    }))
+  }
+
   render() {
     return (
       <div
@@ -22,9 +58,11 @@ class QuestionScreen extends React.Component {
           justifyContent: 'center'
         }}
       >
-        <Subtitle text="Pregunta 1" />
+        <Subtitle text={this.state.currentQuestion.text} />
         <Reactions />
-        <Button text="Next" />
+        <div onClick={this.handleQuestion} style={{ width: '100%' }}>
+          <Button text="Next" />
+        </div>
       </div>
     )
   }
